3b8a139aa3ab10b041afde60eb24b2328219a8d2
Gene C <arch@sapience.com>
2023-03-07 13:11:09 -0500

    Release 1.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'dual-root'
 copyright = '2023, Gene C'
 author = 'Gene C'
-release = '0.9.9'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
